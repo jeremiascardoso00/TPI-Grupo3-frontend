833e2a49ead2bf2b3805822b196e4b042298ee7b
jere <jeremiascardoso00@gmail.com>
2023-09-24 17:05:04 -0300

    adding changes

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class NewsComponent {
 
+  news:any = [
+    [
+      {
+        tittle: "test",
+        message: "a",
+        image: "../../../assets/novedades.jpg"
+      }
+    ],
+    [
+      {
+        tittle: "test",
+        message: "b",
+        image: "../../../assets/novedades.jpg"
+      },
+      {
+        tittle: "test3",
+        message: "c",
+        image: "../../../assets/novedades.jpg"
+      },
+      {
+        tittle: "test",
+        message: "",
+        image: "../../../assets/novedades.jpg"
+      }, 
+    ],
+    [
+      {
+        tittle: "test",
+        message: "d",
+        image: "../../../assets/novedades.jpg"
+      },
+      {
+        tittle: "test",
+        message: "e",
+        image: "../../../assets/novedades.jpg"
+      },
+      {
+        tittle: "test",
+        message: "f",
+        image: "../../../assets/novedades.jpg"
+      }, 
+    ]
+   
+  ]
+
+  constructor() {
+
+  }
+
 }
