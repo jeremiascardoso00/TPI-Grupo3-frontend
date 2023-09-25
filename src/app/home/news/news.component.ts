c215927d0abfdbb57532d0992500afbf9b78891e
jere <jeremiascardoso00@gmail.com>
2023-09-24 22:16:44 -0300

    news secction

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -10,43 +10,37 @@ export class NewsComponent {
   news:any = [
     [
       {
-        tittle: "test",
-        message: "a",
-        image: "../../../assets/novedades.jpg"
-      }
-    ],
-    [
-      {
-        tittle: "test",
-        message: "b",
-        image: "../../../assets/novedades.jpg"
+        tittle: "Primera y segunda semana de marzo",
+        message: "Reuniones de padres con motivo de la presentación de proyectos de sala anuales.",
+        image: "../../../assets/reunion-27-03-9-1000x620.jpg"
       },
       {
-        tittle: "test3",
-        message: "c",
-        image: "../../../assets/novedades.jpg"
+        tittle: "Educacion",
+        message: "Taller virtual para docentes sobre “Lecturas y escrituras, escenarios posibles”",
+        image: "../../../assets/32b6d8645574bba8646d68c9d193129a_M.jpg"
       },
+      
       {
-        tittle: "test",
-        message: "",
-        image: "../../../assets/novedades.jpg"
+        tittle: "Remodelación",
+        message: "Se anuncia la remodelacion de las aulas de computación",
+        image: "../../../assets/aula.png"
       }, 
     ],
     [
       {
-        tittle: "test",
-        message: "d",
-        image: "../../../assets/novedades.jpg"
+        tittle: "Educación Ambiental",
+        message: "Capacitación a docentes para orientar su tarea pedagógica desde la óptica del ecosistema",
+        image: "../../../assets/capacitacion.jpg"
       },
       {
-        tittle: "test",
-        message: "e",
-        image: "../../../assets/novedades.jpg"
+        tittle: "Reunión informativa nivel Pregrado",
+        message: "Invitamos a los/as estudiantes de las carreras de nivel Pregrado, a participar de la reunión «Vida Estudiantil», el martes 16 de septiembre a las 18.30 hs en el Salón de Actos.",
+        image: "../../../assets/students.jpg"
       },
       {
-        tittle: "test",
-        message: "f",
-        image: "../../../assets/novedades.jpg"
+        tittle: "Mes de octubre",
+        message: "Jornadas Anuales Institucionales Interservicio y celebración comunitaria del día de la familia.",
+        image: "../../../assets/students2.jpg"
       }, 
     ]
    
