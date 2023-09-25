bffa2cf27df0d398a4d7ea92264d95814abdfd2c
Vulk <cristiangconci@gmail.com>
2023-09-24 23:14:24 -0300

    change some HTML & CSS files, styles in general

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeModule} from './home/home.module'
 
 const routes: Routes = [
   // { path: 'home', redirectTo: '/home', pathMatch: 'full' },
@@ -12,7 +11,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   },
-  { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 
 ];
 
